1e3747775489c1bfa8b8758b948a006d022a605e
Toru Choi <blueocean239@gmail.com>
2019-11-08 18:42:11 +0800

    Initial commit

---
+++ app/src/main/java/sg/toru/customselector/ui/MainActivity.kt
@@ -0,0 +1,13 @@
+package sg.toru.customselector.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import sg.toru.customselector.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
