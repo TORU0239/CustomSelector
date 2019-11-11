421f9d06c98c2e4418b949a4edf74319e464eb8c
Toru Choi <blueocean239@gmail.com>
2019-11-11 15:17:07 +0800

    added background and toggle state

--- app/src/main/java/sg/toru/customselector/view/CustomSelector.kt
+++ app/src/main/java/sg/toru/customselector/view/CustomSelector.kt
@@ -6,7 +6,9 @@ import android.view.LayoutInflater
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.ContextCompat
 import sg.toru.customselector.R
+import java.lang.IllegalStateException
 
 class CustomSelector : ConstraintLayout {
     constructor(context:Context):super(context){
@@ -32,31 +34,61 @@ class CustomSelector : ConstraintLayout {
 
     private lateinit var typeOfTranportTxt:TextView
     private lateinit var numberOfTransportTxt:TextView
+    private lateinit var image1:ImageView
+    private lateinit var image2:ImageView
+    private lateinit var image3:ImageView
+
 
     private fun init(){
         val view = LayoutInflater.from(context).inflate(R.layout.layout_cusom_selector, this, false)
         addView(view)
 
-        val image1 = view.findViewById<ImageView>(R.id.img_text1)
-        val image2 = view.findViewById<ImageView>(R.id.img_text2)
-        val image3 = view.findViewById<ImageView>(R.id.img_text3)
+        image1 = view.findViewById(R.id.img_text1)
+        image2 = view.findViewById(R.id.img_text2)
+        image3 = view.findViewById(R.id.img_text3)
 
         image1.setOnClickListener {
             currentSelectedSubType = Transport.AIR
             setType()
+            setUnselectedToOtherButton(image1)
         }
         image2.setOnClickListener {
             currentSelectedSubType = Transport.LAND
             setType()
+            setUnselectedToOtherButton(image2)
         }
         image3.setOnClickListener {
             currentSelectedSubType = Transport.SEA
             setType()
+            setUnselectedToOtherButton(image3)
         }
 
         typeOfTranportTxt = view.findViewById(R.id.txt_selector_transport)
         numberOfTransportTxt = view.findViewById(R.id.txt_selector_transport_number)
+
+        image1.performClick()
         setType()
+    }
+
+    private fun setUnselectedToOtherButton(button:ImageView){
+        when(button){
+            image1 ->{
+                image1.background = ContextCompat.getDrawable(context, R.drawable.rect_cornered_box_selected)
+                image2.background = ContextCompat.getDrawable(context, R.drawable.rect_cornered_box_unselected)
+                image3.background = ContextCompat.getDrawable(context, R.drawable.rect_cornered_box_unselected)
+            }
+            image2 ->{
+                image1.background = ContextCompat.getDrawable(context, R.drawable.rect_cornered_box_unselected)
+                image2.background = ContextCompat.getDrawable(context, R.drawable.rect_cornered_box_selected)
+                image3.background = ContextCompat.getDrawable(context, R.drawable.rect_cornered_box_unselected)
+            }
+            image3 ->{
+                image1.background = ContextCompat.getDrawable(context, R.drawable.rect_cornered_box_unselected)
+                image2.background = ContextCompat.getDrawable(context, R.drawable.rect_cornered_box_unselected)
+                image3.background = ContextCompat.getDrawable(context, R.drawable.rect_cornered_box_selected)
+            }
+            else -> throw IllegalStateException("WTF?")
+        }
     }
 
     private fun setType(){
